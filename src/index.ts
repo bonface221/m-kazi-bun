80959e021682d928b13fe51bdddaed7e639ea6da
bonface221 <mainaboniface221@gmail.com>
2024-04-17 12:51:34 +0300

    chaining methods in elysia

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,38 @@ import { Elysia, t } from "elysia";
 import menu from "./ussd";
 import Redis from "ioredis";
 
-
 const REDIS_URL = process.env.REDIS_URL!;
 export const redis = new Redis(6379, REDIS_URL);
 
-
-const app = new Elysia();
-
-app.get('/',()=>"Hello Elysia")
-
-
-app.post('/ussd',({body})=>{
-  //ussd logic
-  const ussdResult = menu.run(body);
-
-
-  return ussdResult;
-},{
-  body:t.Object({
-    sessionId:t.String(),
-    serviceCode:t.String(),
-    phoneNumber:t.String(),
-    networkCode:t.String(),
-    text:t.String()
-  
+const app = new Elysia()
+  .onError(({ code, error }) => {
+    console.error(`Error code: ${code} => ${error}`);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
   })
-})
+  .get("/", () => "Hello Elysia")
+  .post(
+    "/ussd",
+    ({ body }) => {
+      //ussd logic
+      const ussdResult = menu.run(body);
 
+      return ussdResult;
+    },
+    {
+      body: t.Object({
+        sessionId: t.String(),
+        serviceCode: t.String(),
+        phoneNumber: t.String(),
+        networkCode: t.String(),
+        text: t.String(),
+      }),
+    }
+  );
 
-
-app.listen(3000)
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
